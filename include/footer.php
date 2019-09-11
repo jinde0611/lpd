243d609e4ef6d35bab4b839fe62796e40a5bacdd
Jinde <nishant.jinde@here.com>
2019-09-11 12:53:17 +0530

    error

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 
 <nav class="fixed-bottom text-white" style="background-color:#009688">
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+  <div class="footer-copyright text-center py-3" style="line-height:0.3">© 2019 Copyright:
     <a class="text-white" href="https://mdbootstrap.com/education/bootstrap/"> HERE-LPD</a>
   </div>
   <!-- Copyright -->
